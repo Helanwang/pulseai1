85c96df53da8cd409b0fa02ce86dfdfaa66729e3
Shayan Guha <shayanguha@gmail.com>
2024-10-20 15:26:59 -0700

    Add sambanova

--- ai/index.ts
+++ ai/index.ts
@@ -1,8 +1,13 @@
-import { openai } from "@ai-sdk/openai";
 import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
+import { openai, createOpenAI } from '@ai-sdk/openai';
+
+const Xopenai = createOpenAI({
+  basUrl: 'https://api.sambanova.ai/v1',
+  api_key: process.env.SAMBANOVA_API_KEY
+});
 
 export const customModel = wrapLanguageModel({
-  model: openai("gpt-4o"),
+  model: openai("gpt-4o", { apiKey: process.env.SAMBANOVA_API_KEY, baseUrl: 'https://api.sambanova.ai/v1' }),
   middleware: customMiddleware,
 });
